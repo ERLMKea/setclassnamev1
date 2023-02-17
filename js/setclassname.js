1a9fd621714005195c2f127b8caf0ff6d71272d9
ERLMKea <erlm@kea.dk>
2023-02-17 10:32:24 +0100

    setclasname done

--- js/setclassname.js
+++ js/setclassname.js
@@ -7,7 +7,10 @@ let pbSetClassName = document.getElementById("pbSetStyle")
 function setStyle() {
     const elm = document.querySelector("." + inpElementName.value)
     console.log(elm)
+    elm.className = ""
     elm.classList.add(inpStyleName.value)
+    elm.classList.add(inpElementName.value)
+    console.log(elm.classList)
 }
 
 pbSetClassName.addEventListener('click', setStyle)
